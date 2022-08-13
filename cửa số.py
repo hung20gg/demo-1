9bcd5932850eb264c6cf58565747cf8571cf6b80
hung20gg <110990347+hung20gg@users.noreply.github.com>
2022-08-13 19:49:25 +0700

    Add files via upload

---
+++ cửa số.py
@@ -0,0 +1,12 @@
+import tkinter
+
+
+from tkinter import *
+o = Tk()
+o.title("Công cụ tính lãi siêu hiệu quả")
+o.geometry('600x400')
+print("ủa")
+
+
+
+o.mainloop()
--- thử.py
+++ thử.py
@@ -1,1 +1,23 @@
-print("Hng yêu huyn")
+
+
+
+print("Đây là chương trình tính tiền đóng họ")
+Cost= int(input("Giá xe ô tô của bạn là: "))
+current= int(input("Số tiền bạn đang có là: "))
+price= Cost - current
+i= float(input("Lãi suất hàng tháng là: "))
+m= int(input("Thời hạn theo tháng: "))
+
+
+
+
+
+a=1+i
+b=1+i
+
+for n in range(0,m-1):
+    a=a*b
+    n +=1 
+    
+c = (price*a)*i/(a-1)
+print("Tiền lãi mà bạn phải trả hàng tháng là: ",str(round(c,3))," KVNĐ")  
